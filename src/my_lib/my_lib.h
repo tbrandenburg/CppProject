2248f3b06a78d4767c7d0e27f55269252e3c9c3f
Tom Brandenburg <tom.brandenburg@gmx.de>
2022-11-03 13:55:50 +0100

    Added my_fnc to library for documentation test

--- src/my_lib/my_lib.h
+++ src/my_lib/my_lib.h
@@ -13,3 +13,12 @@ int print_hello_world();
  * @return Faculty of n
  */
 unsigned int factorial(unsigned int number);
+
+/**
+ * @brief my_fnc
+ * 
+ * @return 0
+ */
+unsigned char my_fnc(void) {
+    return 0;
+}
